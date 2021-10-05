2fd2cafb68c7e4beb17103a09d1603c91e0fb1de
Jesse Just <87329429+AtlasJust@users.noreply.github.com>
2021-10-05 09:03:00 -0400

    Add files via upload

---
+++ ParadiseInfo.java
@@ -0,0 +1,14 @@
+//chapter 3 demo excercise
+
+public class ParadiseInfo {
+    public static void main(String[] args){
+        //invoke("call") the method called displayInfo()
+        displayInfo();
+
+    }
+    // defination of the displayInfo() method
+    public static void displayInfo(){
+        System.out.println("Paradise Day Spa wants to pamper you");
+        System.out.println("We will make you look good");
+    }
+}
